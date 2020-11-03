d13dfd129ba8f378b4e9b7afd2235ffdb31c9e49
Naga <nagagoutam42@gmail.com>
2020-11-03 08:53:40 +0530

    Part B: Feature: Add comments support for image
    - Also fixed issue with tags and comments when non owner tries to edit/delete

--- ImageHoster/src/main/java/ImageHoster/service/CommentService.java
+++ ImageHoster/src/main/java/ImageHoster/service/CommentService.java
@@ -6,6 +6,8 @@ import ImageHoster.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CommentService {
     @Autowired
@@ -19,5 +21,9 @@ public class CommentService {
     public Comment getComment(Comment comment) {
         System.out.println("GOOOOO gettabbyname tagService");
         return commentRepository.findComment(comment);
+    }
+
+    public List<Comment> getCommentsById(Integer id) {
+        return commentRepository.findCommentsById(id);
     }
 }
